7cd8dfed3d01e31641f6a1d22961734d0555fb06
kamal14rajput <kamal14rajput@gmail.com>
2022-01-11 00:36:58 +0530

    UPDATED: new contact to address book

--- src/com/bridgelabz/AddressBookMain.java
+++ src/com/bridgelabz/AddressBookMain.java
@@ -1,7 +1,65 @@
 package com.bridgelabz;
 
-public class AddressBookMain {
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class AddressBookMain implements IAddressBook {
+
+	Scanner scanner = new Scanner(System.in);
+	ArrayList<Person> personList = new ArrayList<Person>();
+
+	@Override
+	public void add() {
+		System.out.println("Enter your first name");
+		String firstName = scanner.nextLine();
+		System.out.println("Enter your last name");
+		String lastName = scanner.nextLine();
+		System.out.println("Enter your address");
+		String address = scanner.nextLine();
+		System.out.println("Enter your city");
+		String city = scanner.nextLine();
+		System.out.println("Enter your state");
+		String state = scanner.nextLine();
+		System.out.println("Enter your phone");
+		long mobileNo = scanner.nextLong();
+		System.out.println("Enter your zip code");
+		int zip = scanner.nextInt();
+
+		Person person1 = new Person(firstName, lastName, address, city, state, mobileNo, zip);
+		personList.add(person1);
+		System.out.println("Contact added successfully");
+	}
+
+	public void display() {
+		for (int i = 0; i < personList.size(); i++) {
+			Person person = personList.get(i);
+			System.out.println("FirstName:" + person.getFirstName() + "\n" + "LastName:" + person.getLastName() + "\n"
+					+ "Adress:" + person.getAddress() + "\n" + "City:" + person.getCity() + "\n" + "State:"
+					+ person.getCity() + "Phone-Number:" + person.getMobileNo() + "\n" + "Pin-code:"
+					+ person.getPincode());
+		}
+	}
+
 	public static void main(String[] args) {
 		System.out.println("Welcome to address book problem");
+		AddressBookMain adressBookImplementation = new AddressBookMain();
+		boolean condition = true;
+
+		while (condition == true) {
+			Scanner scanner = new Scanner(System.in);
+			System.out.println("1.add" + "\n" + "2.Display");
+			Scanner option = new Scanner(System.in);
+
+			switch (option.nextInt()) {
+			case 1:
+				adressBookImplementation.add();
+				break;
+			case 2:
+				adressBookImplementation.display();
+				break;
+			default:
+				System.out.println();
+			}
+		}
 	}
 }
