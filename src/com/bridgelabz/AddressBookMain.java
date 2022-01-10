a4e62370f4588592c8a01fb8591f6d72830c421c
kamal14rajput <kamal14rajput@gmail.com>
2022-01-11 01:25:40 +0530

    UPDATED: search person in city or state

--- src/com/bridgelabz/AddressBookMain.java
+++ src/com/bridgelabz/AddressBookMain.java
@@ -7,12 +7,14 @@ public class AddressBookMain {
 	public static void main(String[] args) {
 		System.out.println("Welcome to the Address Book Problem");
 		AddressBook adressBookImplementation = new AddressBook();
-		int k = 0;
+		
 		boolean condition = true;
 
 		while (condition == true) {
 			Scanner scanner = new Scanner(System.in);
-			System.out.println("1.add" + "\n" + "2.Display" + "\n" + "3.edit" + "\n" +"4.delete" + "\n" +"5.addMultiplePerson"+ "\n" + "6.duplicateCheck");
+			System.out.println(
+					"1.add" + "\n" + "2.Display" + "\n" + "3.Edit" + "\n" + "4.Delete" + "\n" + "5.Add MultiplePerson"
+							+ "\n" + "6.SearchByName" + "\n" + "7.SearchByState" + "\n" + "8.viewByCity" + "\n" + "SearchPersonInCity");
 			Scanner option = new Scanner(System.in);
 
 			switch (option.nextInt()) {
@@ -23,7 +25,9 @@ public class AddressBookMain {
 				adressBookImplementation.display();
 				break;
 			case 3:
-				adressBookImplementation.edit();
+				System.out.println("Enter the firstName:");
+				String firstName = scanner.nextLine();
+				adressBookImplementation.edit(firstName);
 				break;
 			case 4:
 				System.out.println("Enter the Name of the person do you wants to delete");
@@ -34,12 +38,28 @@ public class AddressBookMain {
 				adressBookImplementation.addMultiplePerson();
 				break;
 			case 6:
-				System.out.println("Enter the Name of the person you wants to check, weather that person is present or not");
+				System.out.println("Enter a Name");
+				String firstName1 = scanner.nextLine();
+				adressBookImplementation.searchPersonByName(firstName1);
+				break;
+			case 7:
+				System.out.println("Enter a name");
+				String state = scanner.nextLine();
+				adressBookImplementation.searchPersonByState(state);
+				break;
+			case 8:
+				System.out.println("Enter the City");
+				String city = scanner.nextLine();
+				adressBookImplementation.viewByCity(city);
+				break;
+
+			case 9:
+				System.out.println("Enter name");
 				String name1 = scanner.nextLine();
-				adressBookImplementation.duplicateCheck(name1);
-                break;
+				adressBookImplementation.searchPersonInCity(name1);
+				break;
 			default:
-				System.out.println("Invalid Input");
+				System.out.println();
 			}
 		}
 	}
