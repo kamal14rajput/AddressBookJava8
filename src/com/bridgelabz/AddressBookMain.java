c445e816f01b4f78c5e3e2034b4da2174241f47b
kamal14rajput <kamal14rajput@gmail.com>
2022-01-11 01:10:25 +0530

    UPDATED: no duplicate entry for same person

--- src/com/bridgelabz/AddressBookMain.java
+++ src/com/bridgelabz/AddressBookMain.java
@@ -5,13 +5,14 @@ import java.util.Scanner;
 public class AddressBookMain {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to address book problem");
+		System.out.println("Welcome to the Address Book Problem");
 		AddressBook adressBookImplementation = new AddressBook();
+		int k = 0;
 		boolean condition = true;
 
 		while (condition == true) {
 			Scanner scanner = new Scanner(System.in);
-			System.out.println("1.add" + "\n" + "2.Display" + "\n" + "3.edit");
+			System.out.println("1.add" + "\n" + "2.Display" + "\n" + "3.edit" + "\n" +"4.delete" + "\n" +"5.addMultiplePerson"+ "\n" + "6.duplicateCheck");
 			Scanner option = new Scanner(System.in);
 
 			switch (option.nextInt()) {
@@ -32,6 +33,11 @@ public class AddressBookMain {
 			case 5:
 				adressBookImplementation.addMultiplePerson();
 				break;
+			case 6:
+				System.out.println("Enter the Name of the person you wants to check, weather that person is present or not");
+				String name1 = scanner.nextLine();
+				adressBookImplementation.duplicateCheck(name1);
+                break;
 			default:
 				System.out.println("Invalid Input");
 			}
