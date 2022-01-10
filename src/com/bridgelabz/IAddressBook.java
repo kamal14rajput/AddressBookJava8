c0d542c2c73c1aedf7ec1d067b5014570f852a36
kamal14rajput <kamal14rajput@gmail.com>
2022-01-11 01:26:26 +0530

    UPDATED: search person in city or state

--- src/com/bridgelabz/IAddressBook.java
+++ src/com/bridgelabz/IAddressBook.java
@@ -5,11 +5,17 @@ public interface IAddressBook {
 
 	void display();
 
-	void edit();
+	void edit(String firstName);
 
 	void delete(String firstName);
-	
+
 	void addMultiplePerson();
-	
-	void duplicateCheck(String firstName);
+
+	void searchPersonByName(String firstname);
+
+	void searchPersonByState(String firstname);
+
+	void viewByCity(String city);
+
+	void searchPersonInCity(String firstName);
 }
